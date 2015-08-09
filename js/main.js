3443ff315a8b5f547539437bc4de70cb5ca97393
elvece <lvcifferello@gmail.com>
2015-08-08 21:40:48 -0600

    added readme and first example problem to main.js and spec.js

--- js/main.js
+++ js/main.js
@@ -3,3 +3,17 @@
 $(document).on('ready', function() {
   console.log('sanity check!');
 });
+
+function largestNumber(arr){
+  var largest = arr[0];
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] > largest) {
+      largest = arr[i];
+    }
+  }
+  return largest;
+}
+
+module.exports = {
+  largestNumber: largestNumber
+};
--- spec/spec.js
+++ spec/spec.js
@@ -6,3 +6,15 @@ var code = require('../js/main.js');
 
 //   });
 // });
+
+
+// Problem 1
+// Write a function that takes three random numbers from 0 to 100 and returns the largest one.
+
+describe('Problem 1', function() {
+  it('should return the largest number', function() {
+    expect(code.largestNumber([10, 20, 30])).toBe(30);
+    expect(code.largestNumber([1, 20, 3])).toBe(20);
+    expect(code.largestNumber([-1, -20, -3])).not.toBe(-20);
+  });
+});
