ffc3784fd634a3cfe37b44ef1a048b9e0cff63f1
elvece <lvcifferello@gmail.com>
2015-08-09 20:29:04 -0600

    added test specs and funcitons to problems 1 and 2, tests pass

--- js/main.js
+++ js/main.js
@@ -1,9 +1,4 @@
-// add scripts
-
-$(document).on('ready', function() {
-  console.log('sanity check!');
-});
-
+//Problem 1
 function largestNumber(arr){
   var largest = arr[0];
   for (var i = 0; i < arr.length; i++) {
@@ -14,6 +9,50 @@ function largestNumber(arr){
   return largest;
 }
 
+//Problem 2
+function oddNumbers(num){
+  var arr = [];
+  if (num >= 40 && num <=100){
+    for (var i = 40; i < num; i++) {
+      if (i % 2 !== 0){
+        arr.push(i);
+      }
+    }
+    return arr;
+  }
+  else {
+    return "Not a number 40 - 100";
+  }
+}
+
+//Problem 3
+function evenNumbers(num){
+  var arr = [];
+  if (num >= 0 && num <=100){
+    for (var i = 0; i < num; i++) {
+      if (i % 2 === 0){
+        arr.push(i);
+      }
+    }
+    return arr;
+  }
+  else {
+    return "Not a number 1 - 100";
+  }
+}
+
+console.log(evenNumbers(10));
+
+
+
+
+
+
+
+
+
 module.exports = {
-  largestNumber: largestNumber
+  largestNumber: largestNumber,
+  oddNumbers: oddNumbers,
+  evenNumbers: evenNumbers
 };
--- spec/spec.js
+++ spec/spec.js
@@ -1,13 +1,5 @@
 var code = require('../js/main.js');
 
-
-// describe('', function() {
-//   it('', function() {
-
-//   });
-// });
-
-
 // Problem 1
 // Write a function that takes three random numbers from 0 to 100 and returns the largest one.
 
@@ -18,3 +10,42 @@ describe('Problem 1', function() {
     expect(code.largestNumber([-1, -20, -3])).not.toBe(-20);
   });
 });
+
+//Problem 2
+// Write a function that takes a random number from 0 to 100 as an argument and returns all the odd numbers from 40 to that random number in an array, if the number is smaller than output the number.<---???
+describe('Problem 2', function() {
+  it('should return all odd numbers from 40 to that random number', function() {
+    expect(code.oddNumbers(20)).toEqual("Not a number 40 - 100");
+    expect(code.oddNumbers(50)).toEqual([41,43,45,47,49]);
+  });
+});
+
+//Problem 3
+// Write a function that takes a random number from 0 to 100 as an argument and returns all the even numbers from 0 to the randomized number in an array.
+describe('Problem 3', function() {
+  it('should return all even numbers until random number', function() {
+    expect(code.evenNumbers(10)).toEqual([0,2,4,6,8]);
+    expect(code.evenNumbers(5)).toEqual([0,2,4]);
+    expect(code.evenNumbers(101)).toEqual("Not a number 1 - 100");
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
